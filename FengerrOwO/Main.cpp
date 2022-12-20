ddb504beab5ce23a8cb995bf0779cfe6e382276a
Fengerros <tadeuszfenger@gmail.com>
2022-12-20 13:53:10 +0100

    grid and cube

--- FengerrOwO/Main.cpp
+++ FengerrOwO/Main.cpp
@@ -23,12 +23,24 @@
 
 GLFWwindow* window;
 
-float width = 800.f;
-float height = 800.f;
+float width = 1000.f;
+float height = 1000.f;
+
+glm::vec3 grid_size = glm::vec3(10, 10, 10); // x y z
+
+std::vector<std::vector<float>> blocks_positions_and_colors;
+
+std::vector<glm::vec3> grid_vertices;
+std::vector<glm::vec2> grid_uvs;
+std::vector<glm::vec3> grid_normals;
 
 std::vector<glm::vec3> FEGO_logo_vertices;
 std::vector<glm::vec2> FEGO_logo_uvs;
 std::vector<glm::vec3> FEGO_logo_normals;
+
+std::vector<glm::vec3> cube_vertices;
+std::vector<glm::vec2> cube_uvs;
+std::vector<glm::vec3> cube_normals;
 
 int main()
 {
@@ -81,20 +93,8 @@ int main()
 
     bool res = loadObj("Assets/FEGO-logo.obj", FEGO_logo_vertices, FEGO_logo_uvs, FEGO_logo_normals);
 
-    GLfloat square2Vertices[] = {
-        //without indices
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		0.5f, -0.5f, 0.0f,  // bottom right
-		0.5f, 0.5f, 0.0f,  // top right
-		
-		0.5f, 0.5f, 0.0f,  // top right
-		-0.5f, 0.5f, 0.0f,  // top left
-		-0.5f, -0.5f, 0.0f  // bottom left
-    };
-	
-
     // Set up vertex buffer objects (VBOs) and vertex array object (VAO)
-    GLuint fego_logo, square2VBO, VAO;
+    GLuint fego_logo, VAO;
     glGenVertexArrays(1, &VAO);
 
     GLfloat* g_color_buffer_data = new GLfloat[FEGO_logo_vertices.size() * 3];
@@ -105,7 +105,6 @@ int main()
     }
 
     glGenBuffers(1, &fego_logo);
-    glGenBuffers(1, &square2VBO);
     glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, fego_logo);
     glBufferData(GL_ARRAY_BUFFER, FEGO_logo_vertices.size() * sizeof(glm::vec3), &FEGO_logo_vertices[0], GL_STATIC_DRAW);
@@ -121,26 +120,56 @@ int main()
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+	
 
-    // Set up the second VAO for the second square
-    GLuint VAO2;
-    glGenVertexArrays(1, &VAO2);
-    glBindVertexArray(VAO2);
+    bool grid_res = loadObj("Assets/grid.obj", grid_vertices, grid_uvs, grid_normals);
 
-    for (int v = 0; v < 12; v++) {
-        g_color_buffer_data[3 * v + 0] = 0;
+    GLuint grid_VBO, grid_VAO;
+
+    glGenVertexArrays(1, &grid_VAO);
+    glBindVertexArray(grid_VAO);
+
+    for (int v = 0; v < grid_vertices.size(); v++) {
+        g_color_buffer_data[3 * v + 0] = 255;
         g_color_buffer_data[3 * v + 1] = 255;
         g_color_buffer_data[3 * v + 2] = 100;
     }
-
-    glBindBuffer(GL_ARRAY_BUFFER, square2VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(square2Vertices), square2Vertices, GL_STATIC_DRAW);
+    glGenBuffers(1, &grid_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, grid_VBO);
+    glBufferData(GL_ARRAY_BUFFER, grid_vertices.size() * sizeof(glm::vec3), &grid_vertices[0], GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
     glGenBuffers(1, &colorbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glBufferData(GL_ARRAY_BUFFER, 12 * 3 * sizeof(GLfloat), g_color_buffer_data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, grid_vertices.size() * 3 * sizeof(GLfloat), g_color_buffer_data, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    bool cube_res = loadObj("Assets/cube.obj", cube_vertices, cube_uvs, cube_normals);
+
+    GLuint cube_VBO, cube_VAO;
+
+    glGenVertexArrays(1, &cube_VAO);
+    glBindVertexArray(cube_VAO);
+
+    for (int v = 0; v < cube_vertices.size(); v++) {
+        g_color_buffer_data[3 * v + 0] = 255;
+        g_color_buffer_data[3 * v + 1] = 0;
+        g_color_buffer_data[3 * v + 2] = 100;
+    }
+    glGenBuffers(1, &cube_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, cube_VBO);
+    glBufferData(GL_ARRAY_BUFFER, cube_vertices.size() * sizeof(glm::vec3), &cube_vertices[0], GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, cube_vertices.size() * 3 * sizeof(GLfloat), g_color_buffer_data, GL_STATIC_DRAW);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
     glEnableVertexAttribArray(1);
 
@@ -149,6 +178,11 @@ int main()
 
     // Set the clear color
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    //cull face
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
 
     // Run the main loop
     do{
@@ -185,8 +219,32 @@ int main()
         glBindVertexArray(0);
 
         // Draw the second square
-        glBindVertexArray(VAO2);
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(grid_VAO);
+        //draw procedural ground with grid
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				ModelMatrix = glm::translate(glm::mat4(1.0), glm::vec3(i * 1, 0, j * 1));
+				MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+				glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+				glUniformMatrix4fv(View, 1, GL_FALSE, &ViewMatrix[0][0]);
+				glUniformMatrix4fv(Model, 1, GL_FALSE, &ModelMatrix[0][0]);
+				glDrawArrays(GL_TRIANGLES, 0, grid_vertices.size());
+			}
+		}
+        glBindVertexArray(0);
+
+		if()
+        // Draw the second square
+        glBindVertexArray(cube_VAO);
+		
+        //draw procedural ground with grid
+         ModelMatrix = glm::translate(glm::mat4(1.0), glm::vec3(2 * 1, 0, 0 * 1));
+         MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+         glUniformMatrix4fv(View, 1, GL_FALSE, &ViewMatrix[0][0]);
+         glUniformMatrix4fv(Model, 1, GL_FALSE, &ModelMatrix[0][0]);
+         glDrawArrays(GL_TRIANGLES, 0, cube_vertices.size());
+
         glBindVertexArray(0);
 
         // Swap the buffers
@@ -197,7 +255,6 @@ int main()
     // Clean up
     glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &fego_logo);
-    glDeleteBuffers(1, &square2VBO);
     glfwTerminate();
 
     return 0;
